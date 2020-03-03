7f24d7cd3b60d352dd86d5cb494a42f0933c1737
ak-front <i.am.alex.kalinin@gmail.com>
2020-03-03 09:57:22 +0300

    Вынес дефолтные значения нотификатора в константу

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,49 +1,41 @@
 import { store } from 'react-notifications-component';
 
+const NOTIFICATION_DEFAULTS = {
+  insert: 'top',
+  container: 'top-right',
+  animationIn: ['animated', 'fadeIn'],
+  animationOut: ['animated', 'fadeOut'],
+  dismiss: {
+    duration: 3000
+  },
+  slidingEnter: {
+    cubicBezier: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    delay: 0,
+    duration: 300
+  },
+  slidingExit: {
+    cubicBezier: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    delay: 0,
+    duration: 600
+  }
+};
+
 export const formatPrice = (price = 0) => {
   return price.toLocaleString('ru-RU');
 };
 
 export const notifySuccess = message => {
   store.addNotification({
+    ...NOTIFICATION_DEFAULTS,
     message,
-    type: 'success',
-    insert: 'top',
-    container: 'top-right',
-    animationIn: ['animated', 'fadeIn'],
-    animationOut: ['animated', 'fadeOut'],
-    dismiss: {
-      duration: 3000
-    },
-    slidingEnter: {
-      duration: 300
-    },
-    slidingExit: {
-      duration: 300
-    }
+    type: 'success'
   });
 };
 
 export const notifyError = message => {
   store.addNotification({
+    ...NOTIFICATION_DEFAULTS,
     message,
-    type: 'danger',
-    insert: 'top',
-    container: 'top-right',
-    animationIn: ['animated', 'fadeIn'],
-    animationOut: ['animated', 'fadeOut'],
-    dismiss: {
-      duration: 3000
-    },
-    slidingEnter: {
-      cubicBezier: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      delay: 0,
-      duration: 300
-    },
-    slidingExit: {
-      cubicBezier: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      delay: 0,
-      duration: 600
-    }
+    type: 'danger'
   });
 };
